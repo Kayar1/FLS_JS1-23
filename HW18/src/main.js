fd5a7230d20f7fe3989d91ddd531a4a2575b5bc9
Kayar <kayar77@gmail.com>
2024-03-13 19:58:18 +0200

    Add files via upload

--- HW18/src/main.js
+++ HW18/src/main.js
@@ -2,45 +2,46 @@ import UI from "./ui.js";
 
 const maxPassLen = 6;
 const minCaseCount = [1, 1, 1, 1]; 
-// A-Z
+const regs = [
+{ value: /[A-Z]/g, minCount: 1, message: 'too small count A-Z<br>'},
+{ value: /[a-z]/g, minCount: 1, message: 'too small count a-z<br>'},
+{ value: /[0-9]/g, minCount: 1, message: 'too small count 0-9<br>'},
+{ value: /\W/gi, minCount: 1, message: 'too small count any symbols<br>'}
 // a-z
 // 0-9
 // ! - ←
 //: - @
 //[ - `
 //{ - ~
+]
+
+const isInRange = (what, from, to) => {
+    return from <= what && what <= to;
+}
 
 function validateForm(info = {loginEmail: '', password: ''}){
     let result = '';
     //check email
-    if (info.loginEmail.indexOf('@') === 0) result += `There are not '@' in email! <br>`;
-    if (info.loginEmail.indexOf('@') !== loginEmail.lastIndexOf('@')) result += `Too many '@' in email! <br>`;
-    if (info.loginEmail.substring(info.loginEmail.indexOf('@') + 1).indexOf('.') === 0)  result += `email is not correct! <br>`;
+    const reEmail = /^(.+)@(.+)\.(.+)$/;
+    if (!reEmail.test(info.loginEmail))  result += `email is not correct! <br>`;
 
     //password
-
-    if (info.password.length <= maxPassLen) result += `Password are too short (min ${maxPassLen} symbols)! <br>`;
-    let caseSymbols = [0, 0, 0, 0];
-    for (let i=0; i<info.password.length; i++){
-        if (65 <= info.password.charCodeAt(i) && info.password.charCodeAt(i) <= 90) caseSymbols[0]++;
-        if (97 <= info.password.charCodeAt(i) && info.password.charCodeAt(i) <= 122) caseSymbols[1]++;
-        if (48 <= info.password.charCodeAt(i) && info.password.charCodeAt(i) <= 57) caseSymbols[2]++;
-        if (33 <= info.password.charCodeAt(i) && info.password.charCodeAt(i) <= 47) caseSymbols[3]++;
-        if (58 <= info.password.charCodeAt(i) && info.password.charCodeAt(i) <= 64) caseSymbols[3]++;
-        if (91 <= info.password.charCodeAt(i) && info.password.charCodeAt(i) <= 96) caseSymbols[3]++;
-        if (123 <= info.password.charCodeAt(i) && info.password.charCodeAt(i) <= 126) caseSymbols[3]++;
-    }
+    const rePass = /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g;
+    if (!rePass.test(info.password))  result += `Password are not in rigth standart! <br>`;
     
-    const checkResult = caseSymbols.reduce((a, b, i) => a += b >= minCaseCount[i] ? 1 : 0, 0);
-    if (checkResult < minCaseCount.length) result += `Password are not in rigth standart! <br>`;
-
+    
+    regs.forEach( el => {
+        const res1 = info.password.match(el.value) ?? []; 
+        if (res1.length < el.minCount) result += el.message;
+    });  
+    
     UI.setErrorLog1(result);
 
     return result === '';
 }
 
 function checkLoginPass(){
-    if (!validateForm(UI.loginEmail, UI.password)){ 
+    if (!validateForm({loginEmail: UI.loginEmail, password: UI.password})){ 
         alert('Wrong loginEmail or password !!!');
     }else{
         alert('All are norm !!!');
